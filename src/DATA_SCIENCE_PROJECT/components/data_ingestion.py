00c03761f57230ad58fec5050eabb5eae8ebb5ae
unknown <aviralrai22@gmail.com>
2025-06-09 11:47:49 +0530

    data ingestion changes

--- src/DATA_SCIENCE_PROJECT/components/data_ingestion.py
+++ src/DATA_SCIENCE_PROJECT/components/data_ingestion.py
@@ -23,9 +23,9 @@ class DataIngestion:
             logging.info("reading completed from mysql database")
             os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
-            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42) # 20% test data and it is random state split wit 40 -40
-            df.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
-            df.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42) # 20% test data and it is random state split with 42
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
             logging.info("data ingestion is completed")
             return(
                 self.ingestion_config.train_data_path,
